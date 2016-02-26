370571fd35fd10dd0fef7b8644076c6b73d3ea20
Jakub Knejzlík <jakub.knejzlik@gmail.com>
2016-02-26 02:43:22 +0100

    cron time update

--- bg.js
+++ bg.js
@@ -1,6 +1,6 @@
 var CronJob = require('cron').CronJob;
 
-var job = new CronJob('*/5 * * * * *',function(){
+var job = new CronJob('0 0 */5 * * *',function(){
     console.log('cron',new Date())
 });
 
